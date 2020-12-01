d376f267c9743878764f4b70da040ec3720b5741
YTYanK <872825564@qq.com>
2020-12-01 15:32:27 +0800

    + 提交 POSD 文件。

---
+++ Pods/Target Support Files/Pods-YTYCoreFramework/Pods-YTYCoreFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YTYCoreFramework : NSObject
+@end
+@implementation PodsDummy_Pods_YTYCoreFramework
+@end
---
+++ Pods/Target Support Files/Pods-YTYCoreFrameworkDome/Pods-YTYCoreFrameworkDome-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YTYCoreFrameworkDome : NSObject
+@end
+@implementation PodsDummy_Pods_YTYCoreFrameworkDome
+@end
